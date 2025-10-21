db01c9eb663551aa5a7ed25460b1e1eafe1bca62
JG1511 <joaotsi2024@gmail.com>
2025-10-21 16:41:10 -0300

    deposito, saque e tranferencia finalizados

--- marcelao/urls.py
+++ marcelao/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name= "index"),
-    path("<int:correntista_id>/", views.extrato_por_correntista, name= "extrato_por_correntista")
+    path("<int:correntista_id>/", views.extrato_por_correntista, name= "extrato_por_correntista"),
+    path("deposito/<int:correntista_id>/", views.realizar_deposito, name= "realizar_deposito")
 ]
--- marcelao/views.py
+++ marcelao/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from .models import VwExtrato
+from .models import VwExtrato, Correntista
+from decimal import Decimal  # Importar para garantir o tipo de dado para Valor
+from django.contrib import messages
+from django.db import transaction, connection
 
 # Create your views here.
 
@@ -22,3 +25,112 @@ def extrato_por_correntista(request,correntista_id):
     lista_extrato = list(dados_extrato.values())
 
     return JsonResponse({'extrato': lista_extrato})
+
+def realizar_deposito(request, correntista_id):
+
+    correntista = Correntista.objects.get(id=correntista_id)
+    
+    context = { 
+        'correntista': correntista
+    }
+
+    if request.method == 'POST':
+        
+        try:
+            valor_str = request.POST.get('valor_deposito', '0') 
+            valor = Decimal(valor_str)  # Convertendo para Decimal
+
+            if valor <= 0:
+
+                messages.error(request, 'O valor do depósito deve ser maior que zero.')
+                return render(request, 'marcelao/form.html', context)
+        
+        except Exception:
+            messages.error(request, 'Valor inválido. Por favor, insira um número válido.')
+            return render(request, 'marcelao/form.html', context)
+
+
+        try:
+            with transaction.atomic(): # serve para garantir que todas as operações dentro do bloco sejam concluídas com sucesso ou nenhuma delas seja aplicada em caso de erro.
+                with connection.cursor() as cursor: # serve para criar um cursor de banco de dados que permite executar comandos SQL diretamente.
+                    
+                    cursor.execute("SELECT sp_deposito(%s, %s)", [correntista_id, valor])
+                    messages.success(request, 'Depósito realizado com sucesso!')
+                    return render(request, 'form.html', context)
+
+        except Exception as e:
+            messages.error(request, f'Erro ao realizar o depósito: {str(e)}')
+            return render(request, 'marcelao/form.html', context)
+
+    else:
+        return render(request, 'marcelao/form.html', context)
+    
+def realizar_saque(request,correntista_id):
+    correntista = Correntista.objects.get(id = correntista_id)
+
+    context = {
+        'correntista': correntista
+    }
+
+    if request.method == 'POST':
+        try:
+            valor_str = request.POST.get('valor_saque', '0') 
+            valor_de_saque = Decimal(valor_str)
+
+            if valor_de_saque <= 0:
+                messages.error(request, 'O valor do saque deve ser maior que zero.')
+                return render(request, 'marcelao/form_saque.html', context)
+        except Exception:
+            messages.error(request, 'Valor inválido. Por favor, insira um número válido.')
+            return render(request, 'marcelao/form_saque.html', context)
+        try:
+            with transaction.atomic(): # o transaction.atomic() serve para garantir que todas as operações dentro do bloco sejam concluídas com sucesso ou nenhuma delas seja aplicada em caso de erro.
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT sp_saque(%s,%s)", [correntista_id, valor_de_saque])
+                    messages.success(request, 'Saque realizado com sucesso!')
+                    return render(request,'marcelao/form_saque.html', context)
+                
+        except Exception as e: # captura qualquer exceção que possa ocorrer durante a execução do bloco try.
+            messages.error(request, f'Erro ao realizar o saque: {str(e)}')
+            return render(request, 'marcelao/form_saque.html', context)
+    else:
+        return render(request, 'marcelao/form_saque.html', context)
+
+def realizar_transferencia(request, correntista_id):
+    correntista = Correntista.objects.get(id=correntista_id)
+
+    context = {
+        'correntista': correntista
+    }
+
+    if request.method == 'POST':
+        try:
+            valor_str = request.POST.get('valor_transferencia', '0') 
+            valor_de_transferencia = Decimal(valor_str)
+            beneficiario_id = int(request.POST.get('beneficiario_id', '0'))
+
+            if valor_de_transferencia <= 0:
+                messages.error(request, 'O valor da transferência deve ser maior que zero.')
+                return render(request, 'marcelao/form_transferencia.html', context)
+            if beneficiario_id <= 0:
+                messages.error(request, 'Beneficiário inválido.')
+                return render(request, 'marcelao/form_transferencia.html', context)
+            
+            if beneficiario_id == correntista_id:
+                messages.error(request, 'Não é possível transferir para si mesmo.')
+                return render(request, 'marcelao/form_transferencia.html', context)
+        
+        except Exception:
+            messages.error(request, 'Valor inválido. Por favor, insira um número válido.')
+            return render(request, 'marcelao/form_transferencia.html', context)
+        
+        try:
+            with transaction.atomic():
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT sp_transferencia(%s,%s,%s)", [correntista_id, beneficiario_id, valor_de_transferencia])
+                    messages.success(request, 'Transferência realizada com sucesso!')
+                    return render(request, 'marcelao/form_transferencia.html', context)
+                
+        except Exception as e:
+            messages.error(request, f'Erro ao realizar a transferência: {str(e)}')
+            return render(request, 'marcelao/form_transferencia.html', context)
